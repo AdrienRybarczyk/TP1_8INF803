6745cea5e8cc35c80b991f1344066c363a318564
AdrienRybarczyk <adrien.rybarczyk@etu.univ-lyon1.fr>
2019-09-27 21:21:21 +0200

    V1

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,73 +1,85 @@
+import org.apache.spark.{SparkConf, SparkContext}
 import org.jsoup.Jsoup
 
-import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+import scala.collection.mutable.ArrayBuffer
 
 object Main extends App {
 
-  //var list =ArrayBuffer[]
+  var tabSpell = ArrayBuffer[Sort]()
 
   for(i <- 1 to 1600) {
     println("Iteration " + i)
     val doc = Jsoup.connect("http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID=" + i).get()
 
-
     ///NAME
     val name = doc.select(".heading p").text()
     //println(name)
-    val listName = List(("name", name))
-
 
     val desc = doc.select(".SPDet")
-    //println(desc)
 
     //Level //////
-
     val data_level = desc.get(0).text()
-    //println(data_level)
     val listLevel = data_level.split("; Level ")(1)
-    //println(listLevel)
-    val listLevelTab = listLevel.split(", ")
-    val tabListLevel = new ArrayBuffer[String]
-    for (i <- 0 until listLevelTab.length) {
-      tabListLevel += listLevelTab(i)
+    var valueLevel = 5
+    if(listLevel.indexOf("sorcerer/wizard")!= -1){
+      val level = listLevel.split("sorcerer/wizard ")(1)
+      valueLevel = level.split(",")(0).toInt
     }
-    val levelList = List(("Level", tabListLevel))
-    //println(levelList)
-
 
     //Component /////
-
     val component = desc.get(2).text()
     //println(component)
 
     val tabComponent = component.split("Components ")
+    //println(tabComponent(1))
     val tabComponentTrunc = tabComponent(1).split(",")
-    val tabComponentArrayBuffer = new ArrayBuffer[String]
-    for (i <- 0 until (tabComponentTrunc.length - 1)) {
-      if(i> 0 & tabComponentTrunc(i).indexOf(" ")!= -1){
+    val componentList = new ArrayBuffer[String]
+    for (i <- 0 until (tabComponentTrunc.length)) {
+      if(i> 0 && tabComponentTrunc(i).indexOf(" ")!= -1){
         tabComponentTrunc(i) = tabComponentTrunc(i).split(" ")(1)
-        //println(tabComponentTrunc(i))
       }
-      tabComponentArrayBuffer += tabComponentTrunc(i)
+      componentList += tabComponentTrunc(i)
     }
-    val componentList = List(("Components", tabComponentArrayBuffer))
     //println(componentList)
-
 
     //Spell Resistance ////
     val data_speelresistance = desc.get(desc.size()-1).text()
     //println(data_speelresistance)
-    var listSpeelResistance = new ListBuffer[String]()
-    if(data_speelresistance.indexOf("Speel Resistance")!= -1){
-      val speelresistance = data_speelresistance.split("; ")(1)
-      //println(listSpeelResistance)
-      listSpeelResistance +=("Speel Resistance",speelresistance.split(" ")(2))
-      //println(tabSpeelResistance)
+    var spellResistance = true
+    if(data_speelresistance.indexOf("Spell Resistance")!= -1){
+      val spellresistance = data_speelresistance.split("Spell Resistance")(1)
+      val valuespellresistance = spellresistance.split(" ")(1)
+      if(valuespellresistance=="yes"){
+        spellResistance = true
+      }else{
+        spellResistance = false
+      }
     }else{
-      listSpeelResistance += ("Speel Resistance","No")
+      spellResistance = false
     }
-    //val arrayBufferElement = new List(listName,levelList,componentList,listSpeelResistance)
-    //list = list + new (name,)
+    tabSpell += new Sort(name,valueLevel,componentList,spellResistance)
   }
 
+ /*for (i <- 0 until tabSpell.length) {
+    println(tabSpell(i).name + " : " + tabSpell(i).level + " : "+ tabSpell(i).componentList + " : "+ tabSpell(i).speelResistance)
+  }*/
+  val conf = new SparkConf()
+    .setAppName("Save Pito")
+    .setMaster("local[*]")
+  val sc = new SparkContext(conf)
+  sc.setLogLevel("ERROR")
+  val test = sc.makeRDD(tabSpell)
+  val resultat = test.collect().filter(e => {
+    e.level <= 4 && e.componentList.contains("V") && e.componentList.length == 1
+  })
+  for (i <- 0 until resultat.length) {
+    println(resultat(i).name + " : " + resultat(i).level + " : "+ resultat(i).componentList + " : "+ resultat(i).speelResistance)
+  }
+println("Total resultat "+ resultat.length)
 }
+
+class Sort (var name: String,
+           var level: Int,
+           var componentList: ArrayBuffer[String],
+           var speelResistance: Boolean
+)extends Serializable
