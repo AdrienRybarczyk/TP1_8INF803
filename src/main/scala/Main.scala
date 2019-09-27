8a8a9dbea6a67ddb0f8112f6c0c4f66a8fc9660c
AdrienRybarczyk <adrien.rybarczyk@etu.univ-lyon1.fr>
2019-09-27 15:57:05 +0200

    Update Crawling Webpage

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,32 +1,73 @@
 import org.jsoup.Jsoup
-import org.jsoup.nodes.{Document, Element}
 
-import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
 object Main extends App {
 
-  //var list =ArrayBuffer[TOTO]
+  //var list =ArrayBuffer[]
 
-  val doc = Jsoup.connect("http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID=1").get()
+  for(i <- 1 to 1600) {
+    println("Iteration " + i)
+    val doc = Jsoup.connect("http://www.dxcontent.com/SDB_SpellBlock.asp?SDBID=" + i).get()
 
-  val name = doc.select(".heading p").text()
-  println(name)
 
-  val desc = doc.select(".SPDet")
-  //println(desc)
+    ///NAME
+    val name = doc.select(".heading p").text()
+    //println(name)
+    val listName = List(("name", name))
 
-  val data_level = desc.get(0).text()
-  //println(data_level)
-  val level = data_level.split("; ")(1)
-  println(level)
 
-  val component = desc.get(2).text()
-  println(component)
+    val desc = doc.select(".SPDet")
+    //println(desc)
 
-  val data_speelresistance = desc.get(6).text()
-  //println(data_speelresistance)
-  val speelresistance = data_speelresistance.split("; ")(1)
-  println(speelresistance)
+    //Level //////
 
-  //list = list + new TOTO(name,)
+    val data_level = desc.get(0).text()
+    //println(data_level)
+    val listLevel = data_level.split("; Level ")(1)
+    //println(listLevel)
+    val listLevelTab = listLevel.split(", ")
+    val tabListLevel = new ArrayBuffer[String]
+    for (i <- 0 until listLevelTab.length) {
+      tabListLevel += listLevelTab(i)
+    }
+    val levelList = List(("Level", tabListLevel))
+    //println(levelList)
+
+
+    //Component /////
+
+    val component = desc.get(2).text()
+    //println(component)
+
+    val tabComponent = component.split("Components ")
+    val tabComponentTrunc = tabComponent(1).split(",")
+    val tabComponentArrayBuffer = new ArrayBuffer[String]
+    for (i <- 0 until (tabComponentTrunc.length - 1)) {
+      if(i> 0 & tabComponentTrunc(i).indexOf(" ")!= -1){
+        tabComponentTrunc(i) = tabComponentTrunc(i).split(" ")(1)
+        //println(tabComponentTrunc(i))
+      }
+      tabComponentArrayBuffer += tabComponentTrunc(i)
+    }
+    val componentList = List(("Components", tabComponentArrayBuffer))
+    //println(componentList)
+
+
+    //Spell Resistance ////
+    val data_speelresistance = desc.get(desc.size()-1).text()
+    //println(data_speelresistance)
+    var listSpeelResistance = new ListBuffer[String]()
+    if(data_speelresistance.indexOf("Speel Resistance")!= -1){
+      val speelresistance = data_speelresistance.split("; ")(1)
+      //println(listSpeelResistance)
+      listSpeelResistance +=("Speel Resistance",speelresistance.split(" ")(2))
+      //println(tabSpeelResistance)
+    }else{
+      listSpeelResistance += ("Speel Resistance","No")
+    }
+    //val arrayBufferElement = new List(listName,levelList,componentList,listSpeelResistance)
+    //list = list + new (name,)
+  }
+
 }
