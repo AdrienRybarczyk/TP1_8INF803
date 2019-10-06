684f5d90c9ab85260ff08fe858629842ebf7b8c2
AdrienRybarczyk <adrien.rybarczyk@etu.univ-lyon1.fr>
2019-10-06 21:15:59 +0200

    PageRank

--- src/main/scala/PageRank.scala
+++ src/main/scala/PageRank.scala
@@ -1,28 +1,43 @@
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
 
 object PageRank extends App {
   val conf = new SparkConf()
-    .setAppName("PageRank")
+    .setAppName("PageRank MapReduce")
+    .setMaster("local[*]")
   val sc = new SparkContext(conf)
   sc.setLogLevel("ERROR")
+  //On crée notre graphe, on aura Nom de la page et la liste des noeuds adjacents
   val pageA = ("PageA",List("PageB","PageC"))
   val pageB = ("PageB",List("PageC"))
   val pageC = ("PageC",List("PageA"))
-  val pageD = ("PageD",List("PageD"))
+  val pageD = ("PageD",List("PageC"))
   val pagerank = 1.0
   var graph = List(
     pageA, pageB, pageC, pageD
   )
- val links = sc.parallelize(graph).partitionBy(new HashPartitioner(4)).persist()
- var ranks = links.mapValues(v => pagerank)
+ val links: RDD[(String, List[String])] = sc.parallelize(graph).partitionBy(new HashPartitioner(4)).persist()
+ var ranks: RDD[(String, Double)] = links.mapValues(v => pagerank)
 
+  println("START")
   for (i <- 1 to 20){
-    val contributions = links.join(ranks).flatMap { case (url, (links, rank)) => links.map(dest => (dest, rank / links.size)) }
+    //On recupere les contributions de chaque noeud entrant et on les ajoute pour obtenir
+    // la somme des contributions des autres noeuds au noeud actuel
+    val contributions: RDD[(String, Double)] = links.join(ranks).flatMap { case (url, (links, rank)) =>
+      links.map(dest => (dest, rank / links.size))
+    }.reduceByKey((x,y)=>(x+y))
     contributions.collect
-    ranks = contributions.reduceByKey((x, y) => x + y).mapValues(v => 0.15 + 0.85*v)
-    ranks.collect
 
+    //On récupère les noeuds n'ayant pas de lien entrant (dans notre cas la page D)
+    val noInlinks = links.subtractByKey(contributions)
+
+    //On ajoute les autres pages et on assigne un pagerank de 0 aux pages qui n'ont pas de contribution
+    val allPages = noInlinks.map(page => (page._1,0.0)).union(contributions)
+
+    //On modifie et sauvegarde la valeur du nouveau pagerank
+    ranks = allPages.mapValues(v => 0.15 + 0.85*v)
   }
-  ranks.foreach(println)
+  println("END")
+  ranks.collect.foreach(println)
 
 }
